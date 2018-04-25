ae87e2fcf47e181a9641bcb48e40affe38ca4aad
Kyle Smith <kyle.smith@ten10.com>
2018-04-25 17:11:45 +0100

    refactored from List of List Ints to List of KVPs

--- TicTacTosh/Brain.cs
+++ TicTacTosh/Brain.cs
@@ -9,12 +9,16 @@ namespace TicTacTosh
         public static void Main(string[] args)
         {
             Board = ClearBoardValues();
-            //Decide who goes first 
-            //DecideWhoGoesFirst();
-            DisplayHelp();
+            AskIfFirstTimePlaying();
+            DecideWhoGoesFirst();
+            do
+            {
+
+            } while (!Victory);
             PrintBoard();
             GetUsersMove();
             UserMakesMove();
+            PrintBoard();
             AIMakeMove();
             PrintBoard();
 
@@ -53,104 +57,81 @@ namespace TicTacTosh
         *  B = Bottom
         */
 
-        public static List<List<int>> corners = new List<List<int>>()
-        {
-            new List<int>() { 1, 1, 0 },
-            new List<int>() { 1, 3, 0 },
-            new List<int>() { 3, 1, 0 },
-            new List<int>() { 3, 3, 0 }
-        };
-
-        public static List<List<int>> midEdges = new List<List<int>>()
-        {
-            new List<int>() { 1, 2, 0 },
-            new List<int>() { 2, 1, 0 },
-            new List<int>() { 2, 3, 0 },
-            new List<int>() { 3, 2, 0 }
-        };
-
-        public static List<List<int>> center = new List<List<int>>()
-        {
-            new List<int>() { 2, 2, 0 }
-        };
+        public static List<KeyValuePair<string, string>> Board { get; set; }
 
         public static Boolean AIGoesFirst { get; set; }
 
-        public static List<int> Position { get; set; }
+        public static KeyValuePair<string, string> Position { get; set; }
 
-        public static List<List<int>> Board { get; set; }
-
-        public static List<List<int>> Corners { get => corners; set => corners = value; }
-
-        public static List<List<int>> MidEdges { get => midEdges; set => midEdges = value; }
-
-        public static List<List<int>> Center { get => center; set => center = value; }
+        public static Boolean Victory = false;
 
         public static void AIMakeMove()
         {
-            List<List<int>> board = new List<List<int>>();
-            List<List<int>> avaliableSpaces = GetAvaliableSpaces();
+            List<KeyValuePair<string, string>> board = new List<KeyValuePair<string, string>>();
+            KeyValuePair<string, string> updatedPos = new KeyValuePair<string, string>();
+
+            List<KeyValuePair<string, string>> avaliableSpaces = GetAvaliableSpaces();
+
             Position = ChooseRandomPosition(avaliableSpaces);
 
-            Position[2] = 1;
-
-            foreach (var pos in Board)
-            {
-                if (pos.ElementAt(0) == Position[0] && pos.ElementAt(1) == Position[1])
+            foreach (var position in Board)
+            { 
+                if (position.Key == Position.Key)
                 {
-                    pos.Insert(2, Position[2]);
+                    updatedPos = new KeyValuePair<string, string>(Position.Key, "X");
+                    board.Add(updatedPos);
                 }
-
-                board.Add(pos);
-
-                Board = board;
+                else
+                {
+                    board.Add(position);
+                }
             }
+
+            Board = board;
+
+            Position = new KeyValuePair<string, string>();
         }
 
-        public static List<List<int>> GetAvaliableSpaces()
+        public static List<KeyValuePair<string, string>> GetAvaliableSpaces()
         {
-            List<List<int>> spacesAvaliable = new List<List<int>>();
-
-            foreach (var pos in Board)
+            List<KeyValuePair<string, string>> spacesAvaliable = new List<KeyValuePair<string, string>>();
+            
+            foreach (var key in Board)
             {
-                if (pos.ElementAt(2) == 0)
+                if (key.Value == " ")
                 {
-                    spacesAvaliable.Add(pos);
+                    spacesAvaliable.Add(key);
                 }
             }
 
             return spacesAvaliable;
         }
 
-        public static List<int> ChooseRandomPosition(List<List<int>> spacesAvaliable)
+        public static KeyValuePair<string, string> ChooseRandomPosition(List<KeyValuePair<string, string>> spacesAvaliable)
         {
             Random rnd = new Random();
 
-            var randomNum = rnd.Next(spacesAvaliable.Count() - 1);
-            var chosenPosition = spacesAvaliable.ElementAt(randomNum);
+            KeyValuePair<string, string> randomPos = new KeyValuePair<string, string>();
 
-            return chosenPosition;
+            var randomNum = rnd.Next(spacesAvaliable.Count() - 1);
+            randomPos = new KeyValuePair<string, string>(spacesAvaliable[randomNum].Key, spacesAvaliable[randomNum].Value);
+
+            return randomPos;
         }
 
-        public static List<List<int>> ClearBoardValues()
+        public static List<KeyValuePair<string, string>> ClearBoardValues()
         {
-            // set values to {1,1,0} {1,2,0} {1,3,0} {2,1,0} {2,2,0} {2,3,0} {3,1,0} {3,2,0} {3,3,0}
-            Board = new List<List<int>>()
+            Board = new List<KeyValuePair<string, string>>()
             {
-                new List<int>() { 1, 1, 0 },
-                new List<int>() { 1, 2, 0 },
-                new List<int>() { 1, 3, 0 },
-                new List<int>() { 2, 1, 0 },
-                new List<int>() { 2, 2, 0 },
-                new List<int>() { 2, 3, 0 },
-                new List<int>() { 3, 1, 0 },
-                new List<int>() { 3, 2, 0 },
-                new List<int>() { 3, 3, 0 }
-            };
-
-            Board =
-            {
-                { { } }
+                new KeyValuePair<string, string>("LT", " "),
+                new KeyValuePair<string, string>("CT", " "),
+                new KeyValuePair<string, string>("RT", " "),
+                new KeyValuePair<string, string>("LM", " "),
+                new KeyValuePair<string, string>("CM", " "),
+                new KeyValuePair<string, string>("RM", " "),
+                new KeyValuePair<string, string>("LB", " "),
+                new KeyValuePair<string, string>("CB", " "),
+                new KeyValuePair<string, string>("RB", " ")
             };
 
             return Board;
@@ -159,67 +140,40 @@ namespace TicTacTosh
         public static void DecideWhoGoesFirst()
         {
             Console.Write("Would you like to go first? (y/n)");
-            string decision = Console.ReadLine();
+            string decision = Console.ReadLine().ToLower();
 
-            if (decision == "y" || decision == "Y")
+            if (decision == "y")
             {
                 AIGoesFirst = false;
-                Console.Write("You will go first.");
+                Console.Write("\nYou will go first.\n");
             }
             else
             {
-                Console.Write("The AI will go first");
+                Console.Write("\nThe AI will go first\n");
                 AIGoesFirst = true;
             }
         }
 
         public static void PrintBoard()
         {
-            int count = 0;
+            Console.Write($"\n\t{Board[0].Value}|{Board[1].Value}|{Board[2].Value}\n");
+            Console.Write($"\t{Board[3].Value}|{Board[4].Value}|{Board[5].Value}\n");
+            Console.Write($"\t{Board[6].Value}|{Board[7].Value}|{Board[8].Value}\n\n");
+        }
 
-            foreach (var pos in Board)
+        public static void AskIfFirstTimePlaying()
+        {
+            string input = "";
+            Console.Write("\nIs this your first time playing? (y/n)");
+            input = Console.ReadLine().ToLower();
+
+            switch (input)
             {
-                if (count == 0)
-                {
-                    Console.Write(" T ");
-                }
-                else if (count == 3)
-                {
-                    Console.Write(" M ");
-                }
-                else if (count == 6)
-                {
-                    Console.Write(" B ");
-                }
-
-                if (pos[2] == 0)
-                {
-                    Console.Write(" ");
-                    count++;
-                }
-                else if (pos[2] == 1)
-                {
-                    Console.Write("X");
-                    count++;
-                }
-                else if (pos[2] == 2)
-                {
-                    Console.Write("O");
-                    count++;
-                }
-
-                if (count == 3 || count == 6)
-                {
-                    Console.Write("\n");
-                }
-                else if (count == 9)
-                {
-                    Console.Write("\n   L C R\n\n");
-                }
-                else
-                {
-                    Console.Write("|");
-                }
+                case "y":
+                    DisplayHelp();
+                    break;
+                default:
+                    break;
             }
         }
 
@@ -229,30 +183,16 @@ namespace TicTacTosh
             Console.Write("\n*\t\t\t    Welcome to the help section!\t\t\t    *");
             Console.Write("\n*************************************************************************************\n");
             Console.Write("*  Each section of the board can be selected by a value as shown in the table below *\n");
+            Console.Write("*  \t\t\t\t\t\t\t\t\t\t    *\n");
             Console.Write("*  T = Top\t\t\t\t\t\t\t\t\t    *\n");
-            Console.Write("*  M = Mid \t\tLT | CT | RT\t __\t O | X | O\t\t\t    *\n"); 
-            Console.Write("*  B = Bot \t\tLM | CM | RM\t __\t X | O | X\t\t\t    *\n"); 
+            Console.Write("*  M = Middle \t\tLT | CT | RT\t __\t O | X | O\t\t\t    *\n"); 
+            Console.Write("*  B = Bottom \t\tLM | CM | RM\t __\t X | O | X\t\t\t    *\n"); 
             Console.Write("*  L = Left \t\tLB | CB | RB\t  \t O | X | O\t\t\t    *\n"); 
             Console.Write("*  C = Center\t\t\t\t\t\t\t\t\t    *\n");
             Console.Write("*  R = Right\t\t\t\t\t\t\t\t\t    *\n");
-            Console.Write("*************************************************************************************\n\n");
+            Console.Write("*************************************************************************************\n");
         }
-
-        /*
-         * T LT|CT|RT
-        *  M LM|CM|RM
-        *  B LB|CB|RB
-        *    L  C  R
-        *    
-        *  Help:
-        *  L = Left
-        *  C = Center
-        *  R = Right
-        *  T = Top
-        *  M = Middle
-        *  B = Bottom
-         */
-
+        
         public static void GetUsersMove()
         {
             Console.Write("Where would you like to go? ");
@@ -261,49 +201,31 @@ namespace TicTacTosh
             switch (input)
             {
                 case "LT":
-                    Position[0] = 1;
-                    Position[1] = 3;
-                    Position[2] = 2;
+                    Position = new KeyValuePair<string, string>("LT", "O");
                     break;
                 case "CT":
-                    Position[0] = 2;
-                    Position[1] = 3;
-                    Position[2] = 2;
+                    Position = new KeyValuePair<string, string>("CT", "O");
                     break;
                 case "RT":
-                    Position[0] = 3;
-                    Position[1] = 3;
-                    Position[2] = 2;
+                    Position = new KeyValuePair<string, string>("RT", "O");
                     break;
                 case "LM":
-                    Position[0] = 1;
-                    Position[1] = 2;
-                    Position[2] = 2;
+                    Position = new KeyValuePair<string, string>("LM", "O");
                     break;
                 case "CM":
-                    Position[0] = 2;
-                    Position[1] = 2;
-                    Position[2] = 2;
+                    Position = new KeyValuePair<string, string>("CM", "O");
                     break;
                 case "RM":
-                    Position[0] = 3;
-                    Position[1] = 2;
-                    Position[2] = 2;
+                    Position = new KeyValuePair<string, string>("RM", "O");
                     break;
                 case "LB":
-                    Position[0] = 1;
-                    Position[1] = 1;
-                    Position[2] = 2;
+                    Position = new KeyValuePair<string, string>("LB", "O");
                     break;
                 case "CB":
-                    Position[0] = 2;
-                    Position[1] = 1;
-                    Position[2] = 2;
+                    Position = new KeyValuePair<string, string>("CB", "O");
                     break;
                 case "RB":
-                    Position[0] = 3;
-                    Position[1] = 1;
-                    Position[2] = 2;
+                    Position = new KeyValuePair<string, string>("RB", "O");
                     break;
                 default:
                     Console.Write("This is not a valid move please try again.");
@@ -313,20 +235,60 @@ namespace TicTacTosh
 
         public static void UserMakesMove()
         {
-            List<List<int>> board = new List<List<int>>();
-            List<List<int>> avaliableSpaces = GetAvaliableSpaces();
+            List<KeyValuePair<string, string>> board = new List<KeyValuePair<string, string>>();
+            KeyValuePair<string, string> updatedPos;
 
-            foreach (var pos in Board)
+            foreach (KeyValuePair<string, string> position in Board)
             {
-                if (pos.ElementAt(0) == Position[0] && pos.ElementAt(1) == Position[1])
+                if (position.Key == Position.Key)
                 {
-                    pos.Insert(2, Position[2]);
+                    updatedPos = new KeyValuePair<string, string>(Position.Key, Position.Value);
+                    board.Add(updatedPos);
                 }
-
-                board.Add(pos);
-
-                Board = board;
+                else
+                {
+                    board.Add(position);
+                }
             }
+            Board = board;
+
+            Position = new KeyValuePair<string, string>();
+        }
+
+        public static Boolean AssessVictoryCondition()
+        {
+            // if any of these are hit 
+            /* 
+            LT
+            LM
+            LB
+           
+            CT
+            CM
+            CB
+
+            RT
+            RM
+            RB
+
+            LT CT RT
+
+            LM CM RM
+
+            LB CB RB
+
+                  RT
+               CM
+            LB
+
+            LR
+               CM
+                  RB
+
+            with the value being either XXX or OOO across them all
+            then set victory to true else
+            */
+            return false;
         }
     }
 }
